347b3ade9418fb41b5b5dbe7b8e6e6d46492a76b
mn-2680-66 <aid268066@saranathan.ac.in>
2025-02-24 15:41:46 +0530

    Update program.py

--- exercise 2B/program.py
+++ exercise 2B/program.py
@@ -1,32 +1,32 @@
-import pandas as pd
 import numpy as np
-data=pd.DataFrame(data=pd.read_csv('data.csv'))
+import pandas as pd
+data=pd.DataFrame(data=pd.read_csv("findS.csv"))
 concepts=np.array(data.iloc[:,:-1])
-print("concepts=",concepts)
+print(concepts)
 target=np.array(data.iloc[:,-1])
-print("target:",target)
+print(target)
 def learn(concepts,target):
-    spec_h=concepts[0].copy()
-    print("initialization of specific and general_h")
-    print("specific_h=",spec_h)
-    general_h=[["?" for i in range(len(spec_h))]for i in range(len(spec_h))]
-    print("general_h",general_h)
+    specific_h=concepts[0].copy()
+    print("Initilizationof specific_h and general_h")
+    print("specific_h=",specific_h)
+    general_h=[["?"for i in range(len(specific_h))]for i in range(len(specific_h))]
+    print("general_h=",general_h)
     for i,h in enumerate(concepts):
         if target[i]=="yes":
-            for x in range(len(spec_h)):
-                if h[x]!= spec_h[x]:
-                     spec_h='?'
-                     general_h[x][x]='?'
-        if target[i]=="no":
-             for x in range(len(spec_h)):
-                if h[x]!= spec_h[x]:
-                     general_h[x][x]=spec_h
+            for x in range(len(specific_h)):
+                if h[x]!=specific_h[x]:
+                    specific_h[x]="?"
+                    general_h[x][x]="?"
+        if target[i]=='no':
+            for x in range(len(specific_h)):
+                if h[x]!=specific_h[x]:
+                    general_h[x][x]=specific_h[x]
                 else:
-                    general_h[x][x]='?'
-        indices=[i for i,val in  enumerate(general_h) if val==['?','?','?','?','?']]
-        for i in indices:
-            general_h.remove(['?','?','?','?','?'])
-        return spec_h,general_h
+                    general_h[x][x]="?"
+    indices=[i for i,val in enumerate(general_h) if val==['?','?','?','?','?','?','?']]
+    for i in indices:
+        general_h.remove(['?','?','?','?','?','?','?'])
+    return specific_h,general_h
 s_final,g_final=learn(concepts,target)
-print("final specfic_h",s_final,sep="\n")
-print("final general_h",g_final,sep="\n")
+print("Final specific_h=",s_final,sep="\n")
+print("Final general_h=",g_final,sep="\n")
